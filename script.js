cd9bf43e560a4f37a5c0cc789dfccb0ee9b4e23f
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-17 23:08:16 +0100

    circle function added || script.js 3 setted

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ window.onload = function () {
         init();
 
     });
+    function Circle(x, y, dx, dy, radius) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+        this.minRadius = radius;
+        this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+        this.draw = function () {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+
+        this.update = function () {
+            if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+                this.dx = -this.dx;
+            }
+            this.x += this.dx;
+
+            if (this.y + 30 > innerHeight || this.y - 30 < 0) {
+                this.dy = -this.dy;
+            }
+            this.y += this.dy;
+
+            //interactivity
+
+            if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+                if (this.radius < maxRadius) {
+                    this.radius += 1;
+                }
+            } else if (this.radius > this.minRadius) {
+                this.radius -= 1;
+            }
+
+
+            this.draw();
+        }
+    }
