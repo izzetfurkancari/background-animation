8d93015f73fb1f75ddf723d2a6a841f7614fa0fa
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-17 22:39:29 +0100

    cı added || script.js 2

--- script.js
+++ script.js
@@ -14,4 +14,21 @@ window.onload = function () {
     window.addEventListener('mousemove', function (event) {
         mouse.x = event.x;
         mouse.y = event.y;
-    })}
+    })};
+    var colorArray = [
+        '#3498DB',
+        '#E74C3C',
+        '#A569BD',
+        '#1ABC9C',
+        '#58D68D',
+        '#F1C40F',
+        '#DC7633'
+    ];
+
+    window.addEventListener('resize', function () {
+        obj.width = window.innerWidth;
+        obj.height = window.innerHeight;
+
+        init();
+
+    });
